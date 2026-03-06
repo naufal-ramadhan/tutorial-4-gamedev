9bfcae48372f050353e6248d4f5800f28699ac29
naufal-ramadhan <naufalnauf04@gmail.com>
2026-03-06 16:22:02 +0700

    tutorial 4 fix formatting.

--- scenes/falling_weight.gd
+++ scenes/falling_weight.gd
@@ -2,8 +2,9 @@ extends RigidBody2D
 
 @export var sceneName = "LoseScreen"
 
+
 func _on_body_entered(body):
 	if body.get_name() == "Player":
 		get_tree().change_scene_to_file(str("res://scenes/" + sceneName + ".tscn"))
 	else:
-		queue_free() 
+		queue_free()
